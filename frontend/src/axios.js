a40fc783ff618b31f54921f2b6e96721edf849b7
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-08-10 14:59:42 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:7892',
+    baseURL: 'https://task-management-secure.onrender.com',
     withCredentials: true
 });
 
