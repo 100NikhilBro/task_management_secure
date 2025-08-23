a12cd413344b763faa68c58280e86b0c90e7cc12
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-08-24 04:03:15 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -16,6 +16,22 @@ exports.createUser = async(req, res) => {
             return res.status(400).json({ message: "Name,email,role, and password are required" });
         }
 
+        if (name.length < 3) {
+      return res.status(400).json({ message: "Name must be at least 3 characters long" });
+    }
+
+ const emailRegex = /^[a-zA-Z0-9._%+-]{4,}@gmail\.com$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Email must be a valid Gmail with at least 4 characters before @gmail.com" });
+    }
+
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        message: "Password must be at least 8 characters long and contain both letters and numbers",
+      });
+    }
+
         const duplicate = await User.findOne({ email });
         if (duplicate) {
             return res.status(409).json({ message: "Email already exists" });
